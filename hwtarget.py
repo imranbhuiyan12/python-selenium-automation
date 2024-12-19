127bf182122342e37e464c13f95a138ff7b22b08
imran bhuiyan <ibhuiyan15@gmail.com>
2024-12-18 23:17:23 -0500

    Update automated search script

--- hwtarget.py
+++ hwtarget.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,19 +15,15 @@ driver.maximize_window()
 #navigate the target .com
 driver.get('https://www.target.com/')
 
-#find element into the search field
-driver.find_element(By.ID,'search').send_keys('tea')
-#from search to click
-driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
 
+driver.find_element(By.XPATH,"//a[@data-test='@web/AccountLink']").click()
+driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
 
 sleep(5)
-# verification
-expected_result = 'tea'
-actual_result = driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
-assert expected_result in actual_result, f"Expected test {expected_result} not in Actual {actual_result}"
 
+expected = 'Sign into your Target account'
+actual = driver.find_element(By.XPATH,"//h1[@class='sc-fe064f5c-0 sc-315b8ab9-2 lnvRvp diHlfH']").text
+assert expected == actual, f"{expected} != {actual}"
 print("test passed")
-
+driver.find_element(By.ID,'login')
 driver.quit()
-
